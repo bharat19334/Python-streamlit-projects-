ec29291c056ebd2bae8a1f859332d403d79fd314
Bharat Goswami <bharatgoswami49200619@gmail.com>
2026-09-04 23:17:50 +0530

    Added PDF Merger project

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from PyPDF2 import PdfMerger
+import io
+
+st.set_page_config(page_title="PDF Merger", page_icon="📄")
+st.title("📄 PDF Merger App")
+uploaded_files = st.file_uploader(
+    "Upload PDF files",
+    type="pdf",
+    accept_multiple_files=True
+)
+if uploaded_files:
+    st.write(f"{len(uploaded_files)} PDFs selected")
+    if st.button("Merge PDFs"):
+        merger = PdfMerger()
+        for pdf in uploaded_files:
+            merger.append(pdf)
+
+        output = io.BytesIO()
+        merger.write(output)
+        merger.close()
+
+        output.seek(0)
+        st.success("PDFs merged successfully!")
+        st.download_button(
+            label="Download Merged PDF",
+            data=output,
+            file_name="merged_pdf.pdf",
+            mime="application/pdf"
+        )
